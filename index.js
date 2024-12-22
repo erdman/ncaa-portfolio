3b2e45576f4417d25d16d39e47f9ac33aa563953
Travis Erdman <travis.erdman@gmail.com>
2024-12-22 00:43:28 -0800

    Adjust new-years-six json and scoring algo to allocate bowl invite bonus properly.  Losers of first round CFP games get the bowl invite bonus there, while all other CFP teams get it in the elite eight (only).

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ function calculateScore(hero, villain, fbs_name_set, game, new_years_six, year) 
     const heroRank = Math.min(hero.teamRank, 26);
     const villainRank = Math.min(villain.teamRank, 26);
     const VILLAIN_FBS = fbs_name_set.has(villain.nameRaw);
+    const HERO_WIN = hero.currentScore > villain.currentScore;
 
-    const score = {win: win_points[hero.currentScore > villain.currentScore][VILLAIN_FBS],
+    const score = {win: win_points[HERO_WIN][VILLAIN_FBS],
                  blowout: VILLAIN_FBS && (hero.currentScore - villain.currentScore >= 30) ? 2 : 0,
                  shutout: VILLAIN_FBS && villain.currentScore === 0 ? 4 : 0,
-                 top25:   top25_points[hero.currentScore > villain.currentScore][heroRank < villainRank][hero.currentScore > villain.currentScore && villainRank === 26 ? 0 : Math.abs(heroRank - villainRank)],
-                 bowl:    (game.week === bowl_week[year] && !new_years_six.championships.has(game.id) ? 10 : 0) +
-                          (game.week === 15 && year >= 2024 && hero.currentScore > villain.currentScore ? 3 : 0) +
-                          (new_years_six.cfp.has(game.id) && hero.currentScore > villain.currentScore ? 3 : 0) +
+                 top25:   top25_points[HERO_WIN][heroRank < villainRank][HERO_WIN && villainRank === 26 ? 0 : Math.abs(heroRank - villainRank)],
+                 bowl:    (game.week === bowl_week[year] && !new_years_six.championships.has(game.id) && !(new_years_six.first_round.has(game.id) && HERO_WIN) ? 10 : 0) +
+                          (game.week === 15 && year >= 2024 && HERO_WIN ? 3 : 0) +
+                          (new_years_six.cfp.has(game.id) && HERO_WIN ? 3 : 0) +
                           (new_years_six.playoffs.has(game.id) ? 6 : 0) +
                           (new_years_six.new_years_fours.has(game.id) ? 3 : 0),
                  displayString:  game.displayString,
