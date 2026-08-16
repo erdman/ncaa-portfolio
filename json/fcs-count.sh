b16d41d482f6825cd4f8ad65eb74462d65b4971f
Travis Erdman <travis.erdman@gmail.com>
2026-08-15 17:30:18 -0700

    Update fcs scripts to work with jq v1.8 which eliminated the --argfile parameter.

--- json/fcs-count.sh
+++ json/fcs-count.sh
@@ -1,6 +1,6 @@
-jq -r --argfile names fbs-names.json '
+jq -r --slurpfile names fbs-names.json '
   .[]
-  | select(.away.nameRaw as $n | $names | index($n) | not)
+  | select(.away.nameRaw as $n | $names[0] | index($n) | not)
   | .home.nameRaw
 ' 202???.json | awk 'NR==FNR {fbs[$0]; next} $0 in fbs {count[$0]++} END {
     for (team in fbs) print team ", " (count[team] ? count[team] : 0)
--- json/fcs-games.sh
+++ json/fcs-games.sh
@@ -6,9 +6,9 @@ exclude_file="$1"
 shift
 
 for data_file in "$@"; do
-  jq -r --argfile names "$exclude_file" '
+  jq -r --slurpfile names "$exclude_file" '
     .[] 
-    | select(.away.nameRaw as $n | $names | index($n) | not)
+    | select(.away.nameRaw as $n | $names[0] | index($n) | not)
     | "\(.home.nameRaw), \(.away.nameRaw)"
   ' "$data_file"
 done
